c81be717d2420ea8e11465c3450f9a0df4655e45
AN_code <Alfayomega5027@gmail.com>
2024-03-26 19:07:48 -0300

    update 26-03-2024

---
+++ src/const/documentation.ts
@@ -0,0 +1,46 @@
+export const html = [
+  {
+    nombre: "MDN",
+    url: "",
+    descripcion: "",
+  },
+  {
+    nombre: "W3C",
+    url: "",
+    descripcion: "",
+  },
+  {
+    nombre: "WHATWG",
+    url: "",
+    descripcion: "",
+  },
+  {
+    nombre: "W3Schools",
+    url: "",
+    descripcion: "",
+  },
+];
+
+export const css = {
+  fuenteUno: {
+    nombre: "",
+    url: "",
+    descripcion: "",
+  },
+  fuenteDos: {
+    nombre: "",
+    url: "",
+    descripcion: "",
+  },
+  fuenteTres: {
+    nombre: "",
+    url: "",
+    descripcion: "",
+  },
+  fuenteCuatro: {
+    nombre: "",
+    url: "",
+    descripcion: "",
+  },
+};
+export const javascript = {};
